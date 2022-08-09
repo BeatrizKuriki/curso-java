453ecd9ac7e3f84291e94864e576631dfaf1a966
BeatrizKuriki <b.f.kuriki@gmail.com>
2022-08-09 17:04:36 -0300

    resolvendo problemas com classes

--- course/src/application/Program.java
+++ course/src/application/Program.java
@@ -2,27 +2,31 @@ package application;
 
 import java.util.Scanner;
 
+import entities.Triangle;
+
 public class Program {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		Scanner sc = new Scanner(System.in);
-		double xA, xB, xC, yA, yB, yC;
+		Triangle x, y;
+		x = new Triangle();
+		y = new Triangle();
 		
 		System.out.println("Enter the measures of triangle X:");
-		xA = sc.nextDouble();
-		xB = sc.nextDouble();
-		xC = sc.nextDouble();
+		x.a = sc.nextDouble();
+		x.b = sc.nextDouble();
+		x.c = sc.nextDouble();
 		
 		System.out.println("Enter the measures of triangle Y:");
-		yA = sc.nextDouble();
-		yB = sc.nextDouble();
-		yC = sc.nextDouble();
-		double p = (xA+xB+xC)/2;
-		double areaX = Math.sqrt(p*(p-xA) * (p-xB)* (p-xC));
+		y.a = sc.nextDouble();
+		y.b = sc.nextDouble();
+		y.c = sc.nextDouble();
+		double p = (x.a+x.b+x.c)/2;
+		double areaX = Math.sqrt(p*(p-x.a) * (p-x.b)* (p-x.c));
 		
-		p = (yA+yB+yC)/2;
-		double areaY = Math.sqrt(p*(p-yA) * (p-yB)* (p-yC));
+		p = (y.a+y.b+y.c)/2;
+		double areaY = Math.sqrt(p*(p-y.a) * (p-y.b)* (p-y.c));
 		
 		System.out.printf("Triangle X area: %.4f%n", areaX);
 		System.out.printf("Triangle Y area: %.4f%n", areaY);
---
+++ course/src/entities/Triangle.java
@@ -0,0 +1,8 @@
+package entities;
+
+public class Triangle {
+	public double a;
+	public double b;
+	public double c;
+
+}
