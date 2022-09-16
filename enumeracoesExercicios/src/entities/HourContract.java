8027c8c6aff7f7a5c25a99d75ae4b77725e9b444
Beatriz Alencar Kuriki <b.f.kuriki@gmail.com>
2022-09-16 11:06:27 -0300

    feat: Worker, Department and HourContract Classes created

---
+++ enumeracoesExercicios/src/entities/HourContract.java
@@ -0,0 +1,45 @@
+package entities;
+
+import java.util.Date;
+
+public class HourContract {
+	
+	private Date date;
+	private double valuePerHour;
+	private int hours;
+	
+	public HourContract() {
+		
+	}
+
+	public HourContract(Date date, double valuePerHour, int hours) {
+		this.date = date;
+		this.valuePerHour = valuePerHour;
+		this.hours = hours;
+	}
+
+	public Date getDate() {
+		return date;
+	}
+
+	public void setDate(Date date) {
+		this.date = date;
+	}
+
+	public double getValuePerHour() {
+		return valuePerHour;
+	}
+
+	public void setValuePerHour(double valuePerHour) {
+		this.valuePerHour = valuePerHour;
+	}
+
+	public int getHours() {
+		return hours;
+	}
+
+	public void setHours(int hours) {
+		this.hours = hours;
+	}
+
+}
