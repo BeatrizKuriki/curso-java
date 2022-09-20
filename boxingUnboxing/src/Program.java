372e1ae989c9d618d7a6b39b7d3d2f39f3cc14b7
BeatrizKuriki <b.f.kuriki@gmail.com>
2022-09-20 10:16:52 -0300

    estudo de Boxing, Unboxing e Wrappers classes

---
+++ boxingUnboxing/src/Program.java
@@ -0,0 +1,15 @@
+
+public class Program {
+
+	public static void main(String[] args) {
+	 int x =20;
+	 Object obj = x;
+	 System.out.println(obj);
+	 
+	 int y = (int) obj;
+	 
+	 System.out.println(y);
+
+	}
+
+}
