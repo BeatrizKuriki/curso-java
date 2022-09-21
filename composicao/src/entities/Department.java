e020b2e0ebd93321561c7880f843439a255f7f34
BeatrizKuriki <b.f.kuriki@gmail.com>
2022-09-21 13:45:19 -0300

    finalizado exercicio

--- composicao/src/entities/Department.java
+++ composicao/src/entities/Department.java
@@ -1,5 +1,19 @@
 package entities;
 
 public class Department {
+	String name;
+	
+	public Department() {
+		
+	}
+	public Department(String name) {
+		this.name = name;
+	}
+	public String getName() {
+		return name;
+	}
 
+	public void setName(String name) {
+		this.name = name;
+	}
 }
--- composicao/src/entities/enums/WorkerLevel.java
+++ composicao/src/entities/enums/WorkerLevel.java
@@ -1,5 +1,8 @@
 package entities.enums;
 
 public enum WorkerLevel {
+	JUNIOR,
+	MID_LEVEL,
+	SENIOR;
 
 }
