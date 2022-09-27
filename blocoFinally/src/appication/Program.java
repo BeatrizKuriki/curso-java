a98c384cfd94d56b1f9d740f47afc0790b59cbdf
BeatrizKuriki <b.f.kuriki@gmail.com>
2022-09-27 16:27:55 -0300

    bloco Finally

---
+++ blocoFinally/src/appication/Program.java
@@ -0,0 +1,25 @@
+package appication;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.Scanner;
+
+public class Program {
+
+	public static void main(String[] args) {
+		File file = new File("C:\\java-libs\\in.txt");
+		Scanner sc = null;
+		try {
+			sc = new Scanner(file);
+			while (sc.hasNextLine()) {
+				System.out.println(sc.nextLine());
+			}
+		} catch (IOException e) {
+			System.out.println("Error opening file: " + e.getMessage());
+		} finally {
+			if (sc != null) {
+				sc.close();
+			}
+		}
+	}
+}
