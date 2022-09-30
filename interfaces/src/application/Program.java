a1623fe0cd56aa366801d57e1e9b47b852513180
Beatriz Alencar Kuriki <b.f.kuriki@gmail.com>
2022-09-30 10:37:58 -0300

    solução sem interface

--- interfaces/src/application/Program.java
+++ interfaces/src/application/Program.java
@@ -1,8 +1,57 @@
 package application;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Locale;
+import java.util.Scanner;
+
+import model.entities.CarRental;
+import model.entities.Vehicle;
+import model.services.BrazilTaxService;
+import model.services.RentalService;
+
 public class Program {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws ParseException {
+		Locale.setDefault(Locale.US);
+		Scanner sc = new Scanner(System.in);
+		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy HH:ss");
+		
+		System.out.println("Enter rental data :");
+		System.out.println("Car model :");
+		String carModel = sc.nextLine();
+		System.out.println("Pickup (dd/MM/yyyy hh:ss): ");
+		Date start = sdf.parse(sc.nextLine());
+		System.out.println("Return (dd/MM/yyyy hh:ss): ");
+		Date finish = sdf.parse(sc.nextLine());
+		
+		CarRental cr = new CarRental(start, finish, new Vehicle(carModel));
+		
+		System.out.println("Enter price per hour: ");
+		double pricePerHour = sc.nextDouble();
+		System.out.println("Enter price per day: ");
+		double pricePerDay = sc.nextDouble();
+		
+		RentalService rentalService = new RentalService(pricePerDay, pricePerHour, new BrazilTaxService());
+		rentalService.processInvoice(cr);
+		
+		System.out.println("INVOICE:");
+		System.out.println("Basic payment: " + String.format("%.2f", cr.getInvoice().getBasicPayment()));
+		System.out.println("Tax: " + String.format("%.2f", cr.getInvoice().getTax()));
+		System.out.println("Total payment: " + String.format("%.2f", cr.getInvoice().getTotalPayment()));
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		sc.close();
 	
 
 	}
--- interfaces/src/model/services/RentalService.java
+++ interfaces/src/model/services/RentalService.java
@@ -1,6 +1,7 @@
 package model.services;
 
 import model.entities.CarRental;
+import model.entities.Invoice;
 
 public class RentalService {
 	
@@ -21,6 +22,24 @@ public class RentalService {
 	}
 	
 	public void processInvoice(CarRental carRental) {
+		long t1 = carRental.getStart().getTime();
+		long t2 = carRental.getFinish().getTime();
+		double hours = (double)(t2 - t1) /1000 /60 /60;
+		
+		
+		double basicPayment;
+		if(hours <= 12.0) {
+			basicPayment = Math.ceil(hours) * pricePerHour;
+		}
+		else {
+			basicPayment = Math.ceil(hours/24) * pricePerDay;
+			
+		}
+		
+		double tax = taxService.tax(basicPayment);
+		
+		carRental.setInvoice(new Invoice(basicPayment, tax));
+		
 		
 	}
 
