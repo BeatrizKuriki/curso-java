d979e750552ded29d53116eed8c6cdafeeb4d0c7
Beatriz Alencar Kuriki <b.f.kuriki@gmail.com>
2022-10-25 09:40:51 -0300

    exercicio finalizado

--- aulaHerancaPolimorfismo/src/entities/Individual.java
+++ aulaHerancaPolimorfismo/src/entities/Individual.java
@@ -27,8 +27,26 @@ public class Individual extends TaxPayer {
 	@Override
 	public double tax() {
 		
+		/*
+		 * TAMBÉM PODEMOS USAR A EXPRESSÃO CONDICIONAL TERNÁRIA;
+		 * O EXEMPLO ABAIXO FICARIA ASSIM:
+		 * double basicTax = (getAnualIncome() < 20000.0 ? getAnualIncome() * 0.15 : getAnualIcome() *0.25;
+		 */
+		double basicTax;
+		if(getAnualIncome() < 20000.0) {
+			basicTax = getAnualIncome() * 0.15;
+		}else {
+			basicTax = getAnualIncome() * 0.25;
+		}
+		basicTax = basicTax - (getHealthExpenditures() * 0.5);
+		if(basicTax < 0.0) {
+			return 0.0;
+			
+		}else {
+			return basicTax;
+		}
+			
 		
-		return 0;
 	}
 
 }
